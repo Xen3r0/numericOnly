0a04881a5d836e40666c325d46d681b90a51c814
Manuel Santisteban <manuel.santisteban@orange.fr>
2013-04-15 20:10:46 +0200

    bug shift+number

--- jquery.numericOnly.js
+++ jquery.numericOnly.js
@@ -16,9 +16,9 @@ $.fn.numericOnly = function(options) {
 	
 	return this.each(function() {
 		$(this).keydown(function(e) {
-			var key = e.charCode || e.keyCode || 0;
+			var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
 			
-			if (key == 8 || key == 9 || key == 46 || (key >= 37 && key <= 40) || (key >= 48 && key <= 57) || (key >= 96 && key <= 105))
+			if (key == 8 || key == 9 || key == 46 || (key >= 37 && key <= 40) || (key >= 48 && key <= 57 && e.shiftKey) || (key >= 96 && key <= 105))
 				return true
 			
 			if (params.decimal) {
